82d332e65070690f2b05566bbe6eccd7d8a2885b
yochna <raoyochna07@gmail.com>
2026-03-04 17:13:58 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ['https://task-manager-ei18.vercel.app', 'http://localhost:3000'],
   credentials: true
 }))
 app.use(express.json())
